e2528ae1d643566638ecec135e3b3b098ab98b75
mfgustav0 <gustavomonteirofavaro@gmail.com>
2024-12-10 23:06:20 -0300

    feat: update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.mfgustavo.nearby"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mfgustavo.nearby"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
